1e90abe9eddbf03c34ebd4d9258b83ce3f41e375
anoop kumar <anoop@xenovus.com>
2016-08-03 14:36:23 +0530

    First commit
    Just created this repository to build the new stat server for backflipt
    application

---
+++ Backflipt_stats/Backflipt_stats/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backflipt_stats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!!");
+        }
+    }
+}
