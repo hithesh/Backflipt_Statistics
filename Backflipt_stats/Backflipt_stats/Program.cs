c53cc6ad6f82402538e21a79bd264750a330ae72
M Hithesh Chowdary <hitheshchm@gmail.com>
2016-08-09 11:20:16 +0530

    Added html display for mongo results

--- Backflipt_stats/Backflipt_stats/Program.cs
+++ Backflipt_stats/Backflipt_stats/Program.cs
@@ -4,10 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using MongoDB.Bson;
 using MongoDB.Driver;
+using MongoDB.Driver.Core;
 using Nancy.Hosting.Self;
 using Nancy;
 using System; 
-
 namespace Backflipt_stats
 {
     public class Program
@@ -19,18 +19,45 @@ namespace Backflipt_stats
             var nancyHost = new Nancy.Hosting.Self.NancyHost(new Uri("http://localhost:9664/"), new DefaultNancyBootstrapper(), hostConfigs);
             nancyHost.Start();
             Console.WriteLine("Web server running !!!");
+            Console.WriteLine("Query results end here!!!");
             Console.ReadLine();
             nancyHost.Stop();
+        }
+        public static async Task testing()
+        {
+            //Console.WriteLine("heyyyy working !!");
+            //var collection = database.GetCollection<BsonDocument>("restaurants");
+            //collection.InsertOne();
+            var filter = Builders<BsonDocument>.Filter.Ne("grades", "");
+            var filter2 = Builders<BsonDocument>.Filter.Eq("cuisine", "Bakery");
+            //var result = await collection.Find(filter).ToListAsync();
+            FieldDefinition<BsonDocument> field = "cuisine";
+            //var x = collection.Distinct(field, filter);
+            //Console.WriteLine(collection.Distinct(""));
         }
     }
     public class HelloModule : NancyModule
     {
+        private static MongoClient client = new MongoClient();
+        private static MongoDB.Driver.IMongoDatabase database = client.GetDatabase("test");
+
         public HelloModule()
-        {
+        { 
+
             //KeyValuePair<dynamic, dynamic> kvp = new KeyValuePair<dynamic, dynamic>();
-            Get["/"] = parameters => "Hello World Testing get";
-            Get["/new"] = paramaeters => "Hello world new!!";
+            Get["/"] = parameters => {
+                List<BsonDocument> l = new List<BsonDocument>();
+                foreach (var x in database.ListCollections().ToList())
+                {
+                    l.Add(x);
+                }
+                var p = "<html>"+l[0].ToString()+l[1].ToString()+"</html>";
+                return p;
+            };
+            Get["/hello/{name}"] = parameters => { return "Hello " + parameters.name; };
             Post["/"] = parameters => {
+                var body = Request.Body.ToString();
+                Console.WriteLine(body);
                 return HttpStatusCode.OK;  //"Hello World testing Post!!!"
                 };
 
